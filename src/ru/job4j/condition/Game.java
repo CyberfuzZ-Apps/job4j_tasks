b6ecaf34e00d785c7cd70f355b74e5d6dc0112cb
CyberfuzZ <keepthelight@icloud.com>
2021-04-11 18:57:28 +0300

    5. Стоит ли играть?

---
+++ src/ru/job4j/condition/Game.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Game {
+    public static int checkGame(double percent, int prize, int pay) {
+        if ((percent * prize) > pay) {
+            return (int) (prize * percent - pay);
+        } else {
+            return 0;
+        }
+    }
+}
