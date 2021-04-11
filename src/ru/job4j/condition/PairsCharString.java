b5887d9229662f647ede1091302b2287ea3a854c
CyberfuzZ <keepthelight@icloud.com>
2021-04-11 20:20:30 +0300

    12. Необычные пары

--- src/ru/job4j/condition/PairsCharString.java
+++ src/ru/job4j/condition/PairsCharString.java
@@ -5,7 +5,7 @@ public class PairsCharString {
         if (l.isEmpty() && r.isEmpty()) {
             return true;
         } else if ((l.charAt(0) == r.charAt(r.length() - 1))
-                && ((l.charAt(l.length()-1) == r.charAt(0)))) {
+                && ((l.charAt(l.length() - 1) == r.charAt(0)))) {
             return true;
         } else {
             return false;
