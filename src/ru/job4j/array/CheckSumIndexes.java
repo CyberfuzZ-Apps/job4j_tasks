4d60c1f0b91a76409001eed637b8482e1bf2a23f
CyberfuzZ <keepthelight@icloud.com>
2021-04-23 14:04:07 +0300

    35. Исключить элементы из двумерного массива.

--- src/ru/job4j/array/CheckSumIndexes.java
+++ src/ru/job4j/array/CheckSumIndexes.java
@@ -1,26 +1,20 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class CheckSumIndexes {
     public static int[] collectNewArray(int[][] data, int sum) {
-        int rslLength = 0;
+        int index = 0;
+        int[] rslArray = new int[data.length * data[0].length];
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[i].length; j++) {
                 if (i + j == sum) {
                     data[i][j] = 0;
                 } else {
-                    rslLength++;
-                }
-            }
-        }
-        int[] rslArray = new int[rslLength];
-        int index = 0;
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[i].length; j++) {
-                if (data[i][j] != 0) {
                     rslArray[index++] = data[i][j];
                 }
             }
         }
-        return rslArray;
+        return Arrays.copyOf(rslArray, index);
     }
 }
