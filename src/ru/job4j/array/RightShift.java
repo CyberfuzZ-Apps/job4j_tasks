fb33a2b22dfeb5ddda51bc0ed8e470cb8c7022b6
CyberfuzZ <keepthelight@icloud.com>
2021-05-01 20:53:49 +0300

    50. Циклический сдвиг вправо

---
+++ src/ru/job4j/array/RightShift.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class RightShift {
+    public static void shift(int[] nums, int count) {
+        for (int i = 0; i < count; i++) {
+            shift(nums);
+        }
+    }
+
+    // метод делает сдвиг с шагом 1
+    private static void shift(int[] nums) {
+        int tmp = nums[nums.length - 1];
+        for (int i = 0; i < nums.length - 1; i++) {
+            nums[nums.length - 1 - i] = nums[nums.length - 2 - i];
+        }
+        nums[0] = tmp;
+    }
+}
