8c567f2435307e532007d7c1ddf31c1052c25a5e
CyberfuzZ <keepthelight@icloud.com>
2021-06-03 17:37:23 +0300

    2. Размер списка и добавление элементов.

---
+++ src/ru/job4j/list/AddElement.java
@@ -0,0 +1,18 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+Задание: необходимо реализовать метод, который добавляет элемент
+в список и сообщает нам удалось это или нет, т.е. метод должен вернуть булево значение.
+Не используйте в методе то, что метод add() возвращает true или false.
+Для этого на входе в метод мы копируем список в новый список, добавляем в тот список,
+который пришел в методе и в конце сравниваем их размеры - если изменился, значит элемент добавился.
+ */
+public class AddElement {
+    public static boolean addNewElement(List<String> list, String str) {
+        List<String> check = new ArrayList<>(list);
+        list.add(str);
+        return check.size() != list.size();
+    }
+}
