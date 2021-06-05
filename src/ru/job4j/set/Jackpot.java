19c91f3c497f54fceda9d69e68fc5091cfcca565
CyberfuzZ <keepthelight@icloud.com>
2021-06-05 17:26:19 +0300

    Set - 5. Джекпот в казино

---
+++ src/ru/job4j/set/Jackpot.java
@@ -0,0 +1,24 @@
+package ru.job4j.set;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+5. Джекпот в казино
+Выигрышной комбинацией для джекпота в казино считается такая,
+в которой все элементы одинаковые.
+
+Метод принимает массив строк, который содержит последовательность элементов игровой комбинации.
+Метод должен вернуть true - если комбинация является выигрышной.
+
+Напомню, что коллекция Set не допускает наличие дубликатов.
+Поэтому если все элементы массива добавить в коллекцию типа Set,
+то размер коллекции будет равен 1 при условии, что комбинация выигрышная.
+ */
+public class Jackpot {
+    public static boolean checkYourWin(String[] combination) {
+        Set<String> jackpot = new HashSet<>(Arrays.asList(combination));
+        return jackpot.size() == 1;
+    }
+}
