35a24d3eb70dca4a66fb4f1bd486c3f9265b7dbb
CyberfuzZ <keepthelight@icloud.com>
2021-06-14 18:34:01 +0300

    3.1. Consumer. Вывод строки. Метод andThen()

---
+++ src/ru/job4j/lambda/ConsumerAndThen.java
@@ -0,0 +1,30 @@
+package ru.job4j.lambda;
+
+import java.util.function.Consumer;
+/*
+3.1. Consumer. Вывод строки. Метод andThen()
+Интерфейс Consumer позволяет, что-то сделать с переданным объектом, при этом ничего не возвращая.
+
+Например,
+Consumer<Integer> con = (num) -> System.out.println(num + 1);
+// при вызове метода accept(1) выведется 2
+
+Consumer также содержит метод andThen(consumer), в который мы можем передать еще один Consumer,
+который выполниться после того, у которого вызывали этот метод.
+
+Например,
+
+Consumer<Integer> con2 = (num) -> System.out.println(num * num);
+
+con.andThen(con2); // con.apply(2) выведет 3 и 4
+
+Ваша задача совместить два Consumer, первый выводит строку через System.out.print,
+а второй приписывает переход на новую строку через System.out.println
+ */
+public class ConsumerAndThen {
+    public static Consumer<String> consumer(String input) {
+        Consumer<String> print = str -> System.out.print(input);
+        Consumer<String> ln = str -> System.out.println();
+        return print.andThen(ln);
+    }
+}
